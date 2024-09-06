bbccfdf4f0a9b7a19a1c0dc6a67a6405ccc237ad
Mike Wallio <michael.wallio@outlook.com>
2024-09-06 14:14:27 -0400

    Add last command time

--- flare.psm1
+++ flare.psm1
@@ -1,11 +1,3 @@
-$ConfigRoot = $env:XDG_CONFIG_HOME ?? $env:LOCAL_APP_DATA ?? "$env:HOME/.cache";
-$ConfigDir = "$ConfigRoot/flare";
-$ConfigFile = "$ConfigDir/flare.ps1";
-
-if (Test-Path $ConfigFile) {
-    . $ConfigFile;
-}
-
 function Get-LinuxDistro {
     $distro = "$(grep '^ID=' /etc/*release | cut -d'=' -f2)".Trim().ToLower();
     if ($distro) {
@@ -33,6 +25,34 @@ function Get-OSIcon {
     return ""
 }
 
-function Prompt {  
-    "$(Get-OSIcon) $($executionContext.SessionState.Path.CurrentLocation)$('>' * ($nestedPromptLevel + 1)) "  
+function Get-LastCommandTime {
+    $lastCommand = Get-History -Count 1;
+    if (-not $lastCommand) { return "" }
+
+    $seconds = ($lastCommand.EndExecutionTime - $lastCommand.StartExecutionTime).TotalSeconds
+    if ($seconds -lt 0.25) { return "" }
+
+    $seconds.ToString("F2") + "s";
+}
+
+$colors = [enum]::GetValues([System.ConsoleColor])
+$flare_promptSeparatorsLeft = "▶"
+$flare_promptHeadLeft = "▶"
+$flare_promptTailLeft = "◖"
+$flare_promptSeparatorsRight = "◀"
+$flare_promptHeadRight = "◀"
+$flare_promptTailRight = "◗"
+
+$flare_osIcon ??= "$(Get-OSIcon)";
+$flare_topPrefix ??= "╭─";
+$flare_bottomPrefix ??= "╰─";
+$flare_promptArrow ??= "";
+$flare_dateFormat ??= 'HH:mm:ss';
+function Prompt {
+    $left = "${flare_topPrefix} ${flare_osIcon} $($executionContext.SessionState.Path.CurrentLocation)";
+    $right = "$(Get-LastCommandTime)$(Get-Date -Format $flare_dateFormat)";
+    $line = "${flare_bottomPrefix}$(${flare_promptArrow} * ($nestedPromptLevel + 1))";
+    $width = $Host.UI.RawUI.WindowSize.Width;
+    $spaces = $width - ($left.Length + $right.Length);
+    "$left$(' ' * $spaces)$right`n$line ";
 }
